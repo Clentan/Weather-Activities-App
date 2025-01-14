a0538c97658b77c39002690192744c6dd3b758cd
Clentan <clentanchauke9089@gmail.com>
2025-01-14 12:12:01 +0200

    .
    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+const { nextui } = require("@nextui-org/react");
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [nextui()],
+};
